17ba8b775670e67ef5e94c4b94c0bcf6c027c0cc
hsoni-devops <hsoni.ps@gmail.com>
2023-09-07 17:43:48 +0530

    update userdatea script

--- userdata-script.sh
+++ userdata-script.sh
@@ -7,6 +7,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 EOF
+cat <<EOF >> .bashrc
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+EOF
 source ~/.bashrc
 nvm install 18.12.1
 # git clone https://github.com/hsoni-devops/Full-stack-expense-app.git
