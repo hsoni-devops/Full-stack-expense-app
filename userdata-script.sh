f28d644844e765a1dac4cd7a1542f7c4ced7119e
hsoni-devops <hsoni.ps@gmail.com>
2023-09-07 17:33:18 +0530

    update userdatea script

--- userdata-script.sh
+++ userdata-script.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 cd /home/ubuntu/
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-sudo DEBIAN_FRONTEND=noninteractive sudo apt-get install docker docker-compose -y
-sudo apt purge nodejs -y
-sudo apt install curl 
+sudo DEBIAN_FRONTEND=noninteractive sudo apt-get install mysql-client -y
 sudo DEBIAN_FRONTEND=noninteractive curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 cat <<EOF >> .bashrc
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 EOF
-bash
+source ~/.bashrc
 nvm install 18.12.1
-sudo DEBIAN_FRONTEND=noninteractive apt-get install npm -y
 git clone https://github.com/hsoni-devops/Full-stack-expense-app.git
 cd Full-stack-expense-app
 git checkout development
-sudo docker-compose up -d
+node --version
 npm i
 npm link razorpay
 npm install pm2@latest -g
-sudo pm2 start app.js
-sudo pm2 startup
-sudo pm2 save
+pm2 start app.js
+pm2 startup
+pm2 save
