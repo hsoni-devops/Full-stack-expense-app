ee1a894b3075f0a4ccbf99ca6a34fd088ce001a2
hsoni-devops <hsoni.ps@gmail.com>
2023-09-07 18:15:19 +0530

    update userdatea script

--- userdata-script.sh
+++ userdata-script.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
+set -x
 DEBIAN_FRONTEND=noninteractive apt-get update -y
-DEBIAN_FRONTEND=noninteractive sudo apt-get install mysql-client -y
-DEBIAN_FRONTEND=noninteractive curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-cat <<EOF >> .bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-EOF
-cat <<EOF >> .bashrc
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-EOF
-source ~/.bashrc
-# nvm install 18.12.1
-# git clone https://github.com/hsoni-devops/Full-stack-expense-app.git
-# cd Full-stack-expense-app
-# git checkout development
-# node --version
-# npm i
-# npm link razorpay
-# npm install pm2@latest -g
-# pm2 start app.js
-# pm2 startup
-# pm2 save
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+DEBIAN_FRONTEND=noninteractive apt-get install nodejs mysql-client -y
