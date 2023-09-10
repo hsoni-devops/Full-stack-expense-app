58d8e5a448dd3aaf59a835ed259b37e9cc9ebbe4
hsoni-devops <hsoni.ps@gmail.com>
2023-09-10 12:41:59 +0530

    change in script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,4 +3,10 @@ cd /home/ubuntu/Full-stack-expense-app
 
 echo "Stopping application"
 
+npm install pm2@latest -g
+git checkout development
 git pull origin development
+aws --region=us-east-1 ssm get-parameter --name "/node-project-poc/enviroment-file"  --output text --query Parameter.Value | awk -F',' '{ for( i=1; i<=NF; i++ ) print $i }' > .env
+node --version
+npm i
+npm link razorpay
