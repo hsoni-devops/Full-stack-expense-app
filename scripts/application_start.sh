8e0d31add09f95f4fa3e2daa8565b98fa04fd9c4
hsoni-devops <hsoni.ps@gmail.com>
2023-09-10 13:55:26 +0530

    update scritps

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 cd /home/ubuntu/Full-stack-expense-app
 
 echo "Restarting application"
-sudo -i PM2_HOME=/etc/.pm2 pm2 start /home/ubuntu/Full-stack-expense-app/app.js
+sudo -i PM2_HOME=/etc/.pm2 pm2 restart 0
 # sudo -i PM2_HOME=/etc/.pm2 pm2 restart all
